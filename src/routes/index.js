9171c807bdb75d60633671d297b2edeb046858c9
mohammadWaleed <mohammed.barghash99@gmail.com>
2023-02-03 17:46:34 +0300

    add react navigation library

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { SafeAreaView, StatusBar, Text } from "react-native";
+
+
+const Main = () => {
+
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>Main Route</Text>
+    </SafeAreaView>
+  );
+
+};
+
+
+export default Main;
